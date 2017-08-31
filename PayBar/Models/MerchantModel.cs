a9b29cbd8b5efa4bdc69c09a6722cffa1fc2da26
payamamirani <the.boy.who.live.70@gmail.com>
2017-08-31 12:05:11 +0430

    A

---
+++ PayBar/Models/MerchantModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PayBar.Models
+{
+    public class MerchantModel
+    {
+        public long ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Family { get; set; }
+
+        public string EnName { get; set; }
+
+        public string EnFamily { get; set; }
+
+        public string FullName { get { return string.Format("{0} {1}", this.Name, this.Family); } }
+
+        public string EnFullName { get { return string.Format("{0} {1}", this.EnName, this.EnFamily); } }
+
+        public string PicUrl { get; set; }
+
+        public string Mcc { get; set; }
+
+        public string CellNo { get; set; }
+
+        public string Address { get; set; }
+    }
+}
